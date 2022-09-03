8df4b2a7a1d4b0120b741b45dd8fe106789f8741
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-09-03 12:23:41 +0200

    Get max interface improvements and changes
    Get max interface now returns an int that tells the caller if it has found a valid interface or not. It also takes in two long * to place the rx bytes and tx bytes of the interface found. Variable name and scope changes.

--- netfetch.c
+++ netfetch.c
@@ -17,47 +17,60 @@ long long_from_file(char *path) {
     return atol(number_str);
 }
 
-void get_max_interface(char *dest) {
-    long current_max = 0;
+int get_max_interface(char *dest, long *dest_rx_bytes, long *dest_tx_bytes) {
+    int res = 0;
+    long current_max = -1;
 
     char *ifs_path = "/sys/class/net";
     DIR *interfaces = opendir(ifs_path);
-
     struct dirent *entry;
 
+    char cur_if_path[MAX_PATH_LENGTH];
+    char cur_opstate_path[MAX_PATH_LENGTH];
+    char cur_opstate[8];
+
+    char cur_rxb_path[MAX_PATH_LENGTH];
+    char cur_txb_path[MAX_PATH_LENGTH];
+
+    long cur_rx_bytes;
+    long cur_tx_bytes;
     while(entry = readdir(interfaces)) {
         if(!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
             continue;
 
-        char cur_if[MAX_PATH_LENGTH];
-        char cur_opstate[MAX_PATH_LENGTH];
-        sprintf(cur_if, "%s/%s", ifs_path, entry->d_name);
-        sprintf(cur_opstate, "%s/%s", cur_if, "operstate");
+        sprintf(cur_if_path, "%s/%s", ifs_path, entry->d_name);
+        sprintf(cur_opstate_path, "%s/%s", cur_if_path, "operstate");
 
-        FILE *opstate_file = fopen(cur_opstate, "r");
+        FILE *opstate_file = fopen(cur_opstate_path, "r");
         if(opstate_file == NULL) {
             printf("%s: no operstate\n");
             continue;
         }
-        char opstate[8];
-        fgets(opstate, 8, opstate_file);
-        if(strcmp(opstate, "up\n")) // If interface is not up, continue
+        fgets(cur_opstate, 8, opstate_file);
+        if(strcmp(cur_opstate, "up\n")) // If interface is not up, continue
             continue;
 
-        char rxb_path[MAX_PATH_LENGTH];
-        char txb_path[MAX_PATH_LENGTH];
-        sprintf(rxb_path, "%s/%s", cur_if, "statistics/rx_bytes");
-        sprintf(txb_path, "%s/%s", cur_if, "statistics/tx_bytes");
-        long cur_rx_bytes = long_from_file(rxb_path);
-        long cur_tx_bytes = long_from_file(txb_path);
+        sprintf(cur_rxb_path, "%s/%s", cur_if_path, "statistics/rx_bytes");
+        sprintf(cur_txb_path, "%s/%s", cur_if_path, "statistics/tx_bytes");
+        cur_rx_bytes = long_from_file(cur_rxb_path);
+        cur_tx_bytes = long_from_file(cur_txb_path);
 
         if(cur_rx_bytes + cur_tx_bytes > current_max) {
             current_max = cur_rx_bytes + cur_tx_bytes;
             strcpy(dest, entry->d_name);
+            *dest_rx_bytes = cur_rx_bytes;
+            *dest_tx_bytes = cur_tx_bytes;
+            res = 1;
         }
     }
+
+    return res;
 }
 
 int main() {
+    char interface[32];
+    long rx = -1, tx = -1;
 
+    get_max_interface(interface, &rx, &tx);
+    printf("%s\nrx: %ld\ntx: %ld\n", interface, rx, tx);
 }
